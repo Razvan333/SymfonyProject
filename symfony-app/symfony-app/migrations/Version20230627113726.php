21d46e3e28e9ee1cc5e78c6aa34481596408ecbb
Razvan pojar <razvan.pojar@emag.ro>
2023-06-27 14:42:42 +0300

    fixed index

--- symfony-app/symfony-app/migrations/Version20230627113726.php
+++ symfony-app/symfony-app/migrations/Version20230627113726.php
@@ -23,7 +23,7 @@ final class Version20230627113726 extends AbstractMigration
         $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, customer_id INT NOT NULL, customer_name VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_81398E099395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE customer_address (id INT AUTO_INCREMENT NOT NULL, customer_id INT DEFAULT NULL, address VARCHAR(255) DEFAULT NULL, INDEX IDX_1193CB3F9395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE customer_address ADD CONSTRAINT FK_1193CB3F9395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (customer_id)');
+        $this->addSql('ALTER TABLE customer_address ADD CONSTRAINT UNIQ_81398E099395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (customer_id)');
     }
 
     public function down(Schema $schema): void
